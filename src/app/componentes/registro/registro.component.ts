5e080fab2a590f1bed77ce9035f3940d19597b98
profesor <profesor>
2017-09-14 20:01:41 -0300

    Segundo commit

---
+++ src/app/componentes/registro/registro.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+// function copiaClave(input: FormControl) {
+  
+//       if (input.root.get('clave') == null) {
+//         return null;
+//       }
+  
+//       const verificar = input.root.get('clave').value === input.value;
+//       return verificar ? null : { mismaClave : true };
+//   }
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  constructor(private builder: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+}
